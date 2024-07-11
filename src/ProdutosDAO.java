122770e634550a04b6b13afbf4f2b197c36063b9
Ygor M. <ygor_frantz@hotmail.com>
2024-07-10 23:34:36 -0300

    adicionado o filtro de produtos vendidos

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -103,7 +103,39 @@ public class ProdutosDAO {
         }
     }
     
-    
+  public ArrayList<ProdutosDTO> listarProdutosVendidos() {
+
+        conn = new conectaDAO().connectDB();
+        ArrayList<ProdutosDTO> produtos = new ArrayList<>();
+
+        try {
+            
+            st = conn.prepareStatement("SELECT * FROM produtos WHERE status = Vendido");
+            rs = st.executeQuery();
+
+            while (rs.next()) {
+                ProdutosDTO p = new ProdutosDTO();
+                p.setId(rs.getInt("id"));
+                p.setNome(rs.getString("nome"));
+                p.setValor(rs.getInt("valor"));
+                p.setStatus(rs.getString("status"));
+
+                produtos.add(p);
+
+            }
+        } catch (SQLException sqle) {
+            JOptionPane.showMessageDialog(null, "Erro ao listar produtos. ERRO: " + sqle.getMessage());
+
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+            }
+        }
+
+        return produtos;
+
+    }  
         
 }
 
